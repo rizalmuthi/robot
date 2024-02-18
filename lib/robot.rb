046577237abad1b94f402f49b5110528ebc0704a
Rizal Muthi <rizalmuthi@gmail.com>
2024-02-18 16:09:03 +1100

    Add robot class with move and report methods

---
+++ lib/robot.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Robot
+  DIRECTIONS = ["NORTH", "EAST", "SOUTH", "WEST"]
+
+  def initialize(x_coord: 0, y_coord: 0, direction: "NORTH")
+    @x_coord = x_coord
+    @y_coord = y_coord
+    @direction = direction
+  end
+
+  def move
+    case direction
+    when "NORTH" then move_north
+    when "SOUTH" then move_south
+    when "EAST" then move_east
+    when "WEST" then move_west
+    end
+  end
+
+  def report
+    {
+      x_coord: x_coord,
+      y_coord: y_coord,
+      direction: direction
+    }
+  end
+
+  private
+  attr_reader :x_coord, :y_coord, :direction
+
+  def move_north
+    @y_coord += 1
+  end
+
+  def move_south
+    @y_coord -= 1
+  end
+
+  def move_east
+    @x_coord += 1
+  end
+
+  def move_west
+    @x_coord -= 1
+  end
+
+end
---
+++ spec/robot_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require './lib/robot'
+
+describe Robot do
+  let(:x_coord) { 1 }
+  let(:y_coord) { 1 }
+  let(:direction) { "NORTH" }
+  let(:robot) { Robot.new(x_coord: x_coord, y_coord: y_coord, direction: direction) }
+
+  describe "#move" do
+    context "when the direction is NORTH" do
+      it "increases the y coordinate" do
+        robot.move
+
+        report = robot.report
+
+        expect(report[:x_coord]).to eq(x_coord)
+        expect(report[:y_coord]).to eq(y_coord + 1)
+        expect(report[:direction]).to eq(direction)
+      end
+    end
+
+    context "when the direction is SOUTH" do
+      let(:direction) { "SOUTH" }
+
+      it "decreases the y coordinate" do
+        robot.move
+
+        report = robot.report
+
+        expect(report[:x_coord]).to eq(x_coord)
+        expect(report[:y_coord]).to eq(y_coord - 1)
+        expect(report[:direction]).to eq(direction)
+      end
+    end
+
+    context "when the direction is EAST" do
+      let(:direction) { "EAST" }
+
+      it "increases the x coordinate" do
+        robot.move
+
+        report = robot.report
+
+        expect(report[:x_coord]).to eq(x_coord + 1)
+        expect(report[:y_coord]).to eq(y_coord)
+        expect(report[:direction]).to eq(direction)
+      end
+    end
+
+    context "when the direction is WEST" do
+      let(:direction) { "WEST" }
+
+      it "decreases the x coordinate" do
+        robot.move
+
+        report = robot.report
+
+        expect(report[:x_coord]).to eq(x_coord - 1)
+        expect(report[:y_coord]).to eq(y_coord)
+        expect(report[:direction]).to eq(direction)
+      end
+    end
+  end
+
+  describe "#report" do
+    it "returns the current data of the robot" do
+      report = robot.report
+
+      expect(report[:x_coord]).to eq(x_coord)
+      expect(report[:y_coord]).to eq(y_coord)
+      expect(report[:direction]).to eq(direction)
+    end
+  end
+end
